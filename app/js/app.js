008382f3a1f4d136270b6ab7d8a7f45dab2f032f
Dmitry Markov <forall@stylive.com>
2016-05-12 18:16:37 +0300

    home page almost ready

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+function init() {
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible
+  $('.collapsible').collapsible();
+}
+
+$(document).ready(function() {
+  init();
+});
